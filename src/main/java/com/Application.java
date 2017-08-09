62fa35581990ac30697e18234404a4fbf0fdb759
yangqing <736875071@qq.com>
2017-08-09 13:01:06 +0800

    pom 项目名称修改

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
